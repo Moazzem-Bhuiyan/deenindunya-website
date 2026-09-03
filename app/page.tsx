2531d471a3c5d35608e20041651a270622e6881e
Moazzem Bhuiyan <tiyonbhuiyan.201@gmail.com>
2026-09-03 10:50:41 +0600

    Comment all page

--- app/page.tsx
+++ app/page.tsx
@@ -1,27 +1,28 @@
-import Navbar from "@/components/navbar/Navbar";
-import HeroSection from "@/components/hero/HeroSection";
-import IntroSection from "@/components/lifestyle/IntroSection";
-import FeatureGrid from "@/components/features/FeatureGrid";
-import ScreenshotShowcase from "@/components/screenshots/ScreenshotShowcase";
-import LifestyleSection from "@/components/lifestyle/LifestyleSection";
-import AudienceSection from "@/components/audience/AudienceSection";
-import FinalCTA from "@/components/download/FinalCTA";
-import Footer from "@/components/footer/Footer";
+// import Navbar from '@/components/navbar/Navbar';
+// import HeroSection from '@/components/hero/HeroSection';
+// import IntroSection from '@/components/lifestyle/IntroSection';
+// import FeatureGrid from '@/components/features/FeatureGrid';
+// import ScreenshotShowcase from '@/components/screenshots/ScreenshotShowcase';
+// import LifestyleSection from '@/components/lifestyle/LifestyleSection';
+// import AudienceSection from '@/components/audience/AudienceSection';
+// import FinalCTA from '@/components/download/FinalCTA';
+// import Footer from '@/components/footer/Footer';
 
 export default function Home() {
   return (
     <>
-      <Navbar />
-      <main>
-        <HeroSection />
+      {/* <Navbar /> */}
+      <div className="flex flex-col items-center justify-center min-h-screen py-2 border">
+        {/* <HeroSection />
         <IntroSection />
         <FeatureGrid />
         <ScreenshotShowcase />
         <LifestyleSection />
         <AudienceSection />
-        <FinalCTA />
-      </main>
-      <Footer />
+        <FinalCTA /> */}
+        Hello, Deen in Dunya!
+      </div>
+      {/* <Footer /> */}
     </>
   );
 }
